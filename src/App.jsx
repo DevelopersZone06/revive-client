2cfed604312a314e30ff27b01eeb32130cab508b
DevelopersZone06 <DevelopersZone06@gmail.com>
2024-01-16 08:59:54 +0600

    initial basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       
+      </div>
+      <h1 className='text-6xl'>Hello world</h1>
+      <h2> Hi there, It's me faysal hosen </h2>
+    
+    </>
+  )
+}
+
+export default App
