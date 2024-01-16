8c5bbdbd61928cd0e7c4592398a865fb482d3885
DevelopersZone06 <DevelopersZone06@gmail.com>
2024-01-16 22:48:07 +0600

    home page added

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Root from './Layout/Root/Root';
 import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
 
 const router = createBrowserRouter([
   {
@@ -14,14 +15,15 @@ const router = createBrowserRouter([
     element: <Root></Root>,
     children : [
       {
+        path: '/',
+        element:<Home/>
+      }, {
         path: '/login',
         element:<Login></Login>
       }
     ]
   },
-  {
 
-  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
