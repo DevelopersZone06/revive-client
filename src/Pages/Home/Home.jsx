5b9344f893aacd427afa7773742c9ace0fef0ace
faysalhosen <faysalhosen7025@gmail.com>
2024-01-18 16:37:26 +0600

    footer design added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,11 @@
 import Banner from "../../Components/HomeComponents/Banner";
-
 import Footer from "../Footer/Footer";
 
 const Home = () => {
     return (
         <div>
-          <Banner></Banner>  
-
-            <h1>this is home</h1>
-            <Footer/>
+            <Banner></Banner>
+            <Footer />
         </div>
     );
 };
