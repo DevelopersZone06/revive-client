da68ae74b47bcb2af94d976e22469e2abeabd20a
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2024-01-19 15:59:35 +0600

    designed the packages section for home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
-import Banner from "../../Components/HomeComponents/Banner";
+import Banner from "../../Components/HomeComponents/Banner/Banner";
 import Clients from "../../Components/HomeComponents/Client/Clients";
 import Features from "../../Components/HomeComponents/Features";
-import Packages from "../../Components/HomeComponents/Packages";
+import Packages from "../../Components/HomeComponents/Packages/Packages";
 
 import Footer from "../Footer/Footer";
 
