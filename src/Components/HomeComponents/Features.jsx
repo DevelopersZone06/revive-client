2019fe2e1b07e4d1673ed8d914c10c0bbf624042
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-19 11:10:24 +0600

    add a Card component for features section

--- src/Components/HomeComponents/Features.jsx
+++ src/Components/HomeComponents/Features.jsx
@@ -8,7 +8,7 @@ const Features = () => {
     return (
         <div className="my-16">
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-14'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-14 mx-6'>
                 {/* card-1 */}
                 <div className='shadow-2xl before:block before:-left-1 before:-top-1 before:bg-[#448C74] before:absolute before:h-0 before:w-0 before:hover:w-[100%] before:hover:h-[100%]  before:duration-500 before:-z-40 after:block after:-right-1 after:-bottom-1 after:bg-[#448C74] after:absolute after:h-0 after:w-0 after:hover:w-[100%] after:hover:h-[100%] after:duration-500 after:-z-40 bg-white relative inline-block rounded-md hover:shadow-2xl'>
                     <div className='py-6 text-center'>
