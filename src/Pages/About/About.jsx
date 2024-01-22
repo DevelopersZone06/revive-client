0b37c29776c99030eaeb9dffb3e76d45c149be2f
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-22 12:52:36 +0600

    add why chose us section about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,11 +2,13 @@ import AboutTitle from "./AboutTitle";
 import image1 from '../../assets/about/about_1.jpg'
 import image2 from '../../assets/about/about_2.jpg'
 import { IoCheckmarkDone } from "react-icons/io5";
+import Title from "../../Shared/Title";
+import { GrYoga } from "react-icons/gr";
 const About = () => {
     return (
         <div>
             <AboutTitle />
-{/* 1st section */}
+            {/* 1st section */}
             <div className="flex flex-col sm:flex-row md:flex-row mx-6 mt-14 gap-10">
                 <img src={image2} alt="" className="w-[410px] md:w-[300px] rounded-md object-cover border-t-8 border-l-8 border-[#448c74] transition delay-150 duration-300 ease-in-out hover:scale-90" />
                 <div>
@@ -33,14 +35,36 @@ const About = () => {
             </div>
             {/* second section */}
             <div className="flex flex-col sm:flex-row md:flex-row justify-between mx-6 mt-14 gap-10">
-                
+
                 <div>
                     <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">Our <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-[#448c74] relative inline-block"><span className="relative text-white">misssion</span></span></h2>
                     <p className="max-w-2xl py-7 text-lg text-justify indent-16">Our personal trainers can help you meet your fitness goals. They can become your teacher, your motivator, your coach and your friend. Our personal trainers are degreed and certified by an accredited fitness orga-nization.</p>
                     <p className="max-w-2xl py-5 text-lg text-justify indent-16">Our personal trainers can help you meet your fitness goals. They can become your teacher, your motivator, your coach and your friend. Our personal trainers are degreed and certified by an accredited fitness orga-nization.</p>
-                    
+
                 </div>
                 <img src={image1} alt="" className="md:w-[500px] rounded-md object-cover transition delay-150 duration-300 ease-in-out hover:scale-90" />
+            </div>
+            {/* third section */}
+
+            <div>
+                <div className="paralax-item  bg-fixed my-14 py-7">
+                    <Title subHeading={'Discover the Reasons'} heading={'Why Chose Us'} />
+                    <div>
+                       <div className="mx-6 grid grid-cols-3 gap-5">
+                       <div className="flex gap-5 py-3">
+                        <GrYoga className="h-16 w-16 rounded-full p-1 bg-[#448c74] text-[#E5C466] "/>
+                            <div className="justify-start">
+                               
+                               
+                                <h3 className="text-lg font-semibold">Experinced Coachers</h3>
+                               <p className="max-w-xs">We’re here to help you overcome the barriers in the way to a ...</p>
+                            </div>
+                           
+                        </div>
+                      
+                       </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
