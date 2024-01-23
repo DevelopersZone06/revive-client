fe50092cc299fe772debf3781d1d05d40c2e2bef
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-23 08:43:20 +0600

    add pagination button of gallery page

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -9,6 +9,16 @@ const Gallery = () => {
             <GalleryTitle/>
             
            <ButtonGallery/>
+
+           {/* pagination system*/}
+           
+           <div className="flex justify-center items-center my-14">
+            <button className="border-secondary px-10 py-2 uppercase text-xl font-semibold secondary-color hover:bg-[#448c74] hover:text-white">1</button>
+           
+            <button className="border-secondary px-10 py-2 uppercase text-xl font-semibold secondary-color hover:bg-[#448c74] hover:text-white">2</button>
+            <button className="border-secondary px-10 py-2 uppercase text-xl font-semibold secondary-color hover:bg-[#448c74] hover:text-white">...</button>
+            <button className="border-secondary px-10 py-2 uppercase text-xl font-semibold secondary-color hover:bg-[#448c74] hover:text-white">10</button>
+            </div>
         </div>
     );
 };
