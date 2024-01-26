7e39ad158ef61229c86a937b5e48d1a1202ffbad
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2024-01-26 14:56:58 +0600

    added the services section in home page

--- src/Pages/UsersServices/AllServices/AllServices.jsx
+++ src/Pages/UsersServices/AllServices/AllServices.jsx
@@ -4,8 +4,8 @@ import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import Loading from "../../../Shared/Loading/Loading";
 import Title from "../../../Shared/Title";
 
-
 import ParallaxForBMI from "../../../Components/ServicesComponents/ParallaxForBMI/ParallaxForBMI";
+import AllServiceTitle from "./AllServiceTitle";
 
 const AllServices = () => {
   const axiosPublic = useAxiosPublic();
@@ -17,175 +17,187 @@ const AllServices = () => {
     },
   });
   if (isPending) return <Loading></Loading>;
-  const trainer = e => {
-    const val = e.target.value
-    console.log(val)
-}
+  const trainer = (e) => {
+    const val = e.target.value;
+    console.log(val);
+  };
   return (
-    <div className="px-[2%] sm:px-[5%] lg:px-[8%] py-28">
-      {/* Special BMI */}
+    <>
+      <AllServiceTitle></AllServiceTitle>
+      <div className="px-[2%] sm:px-[5%] lg:px-[8%] py-28">
+        {/* Special BMI */}
 
-      <ParallaxForBMI></ParallaxForBMI>
-      <div className="mt-36 -mb-5">
-        <Title subHeading={"Find Your Fitness Goal With Us"}></Title>
-      </div>
+        <ParallaxForBMI></ParallaxForBMI>
+        <div className="mt-36 -mb-5">
+          <Title subHeading={"Find Your Fitness Goal With Us"}></Title>
+        </div>
 
-      {/* Search and filter  */}
-      <div className="flex  items-center md:flex-row flex-col mb-6">
-        <div className="md:w-3/4 w-full">
-          <form
-            action="
+        {/* Search and filter  */}
+        <div className="flex  items-center md:flex-row flex-col mb-6">
+          <div className="md:w-3/4 w-full">
+            <form
+              action="
         "
-          >
-            <div className="flex items-center  gap-10  ">
-              <input
-                type="search"
-                placeholder="Search In Revive"
-                className="rounded-md px-10 py-2 border-2 border-[#448c74] w-full"
-              />
-              {/* <div className="text-2xl  ">
+            >
+              <div className="flex items-center  gap-10  ">
+                <input
+                  type="search"
+                  placeholder="Search In Revive"
+                  className="rounded-md px-10 py-2 border-2 border-[#448c74] w-full"
+                />
+                {/* <div className="text-2xl  ">
                 <IoMdSearch className=""></IoMdSearch>
               </div> */}
-            </div>
-          </form>
-        </div>
-        <div className="bg-white text-center  p-5 space-y-2 rounded-md flex gap-2">
-          <h1 className="font-semibold">Sort By Price</h1>
-          <select
-            name="trainer"
-            id="trainer"
-            className="w-full primary-bg text-center py-1 rounded-md outline-0"
-          >
-            <option disabled selected value="">
-              Select
-            </option>
-            <option value="low">Low To High</option>
-            <option value="high">High To Low</option>
-          </select>
-        </div>
-      </div>
-  {/* All services with side ber */}
-      <div className="sm:flex gap-5">
-
-      <div className="sm:w-[250px] primary-bg p-5 border-primary rounded-l-md">
-                    <h1 className="text-2xl font-semibold border-b border-b-white pb-1">Filter Services</h1>
-                    <div className="grid grid-cols-2 gap-5 sm:block">
-                        <div className="bg-white text-center mt-8 p-5 space-y-2 rounded-md">
-                            <h1 className="font-semibold">Filter By Trainer</h1>
-                            <select onChange={trainer} name="trainer" id="trainer" className="w-full primary-bg text-center py-1 rounded-md outline-0">
-                                <option selected disabled value="disable">Select</option>
-                                {services.map(service=>(
-                                    <option key={service._id} value={service.trainer}>Jhon doe</option>
-                                ))}
-                              
-                            </select>
-                        </div>
-
-                     
-                        {/*Category  */}
-                        <h1 className="text-2xl font-semibold border-b border-b-white pb-1 mt-8  ">Filter Category</h1>
-                        <div className="gap-2 grid-cols-2 grid  p-5">
-          <button
-            className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
-            // onClick={() => handleCategory("All")}
-          >
-            All
-          </button>
-          <button
-            className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
-            // onClick={() => handleCategory("All")}
-          >
-            Yoga
-          </button>
-          <button
-            className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
-            // onClick={() => handleCategory("Kitchen")}
-          >
-           Weight Loss
-          </button>
-          <button
-            className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
-            // onClick={() => handleCategory("Bedroom")}
-          >
-          Nutrition
-          </button>
-          <button
-            className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
-            // onClick={() => handleCategory("Living Room")}
-          >
-          Fitness
-          </button>
-        </div>
-        <div>
-          <div >
-            <h3 className="text-2xl font-semibold border-b border-b-white pb-1 mt-8 ">Filter Course Duration</h3>
-            <div className="p-5">
-            <div >
-              <input
-                type="radio"
-                name="price"
-                value="15-30"
-                id="regular"
-             
-                // onChange={onOptionChange}
-              />
-              <label htmlFor="regular">15-30</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="price"
-                value="30-50"
-                id="medium"
-              
-                // onChange={onOptionChange}
-              />
-              <label htmlFor="medium">30-50</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="price"
-                value="50-80"
-                id="large"
-                
-                // onChange={onOptionChange}
-              />
-              <label htmlFor="large">50-80</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="price"
-                value="80-100"
-                id="extra-large"
-               
-                // onChange={onOptionChange}
-              />
-              <label htmlFor="extra-large">80-100</label>
-            </div>
+              </div>
+            </form>
           </div>
+          <div className="bg-white text-center  p-5 space-y-2 rounded-md flex gap-2">
+            <h1 className="font-semibold">Sort By Price</h1>
+            <select
+              name="trainer"
+              id="trainer"
+              className="w-full primary-bg text-center py-1 rounded-md outline-0"
+            >
+              <option disabled selected value="">
+                Select
+              </option>
+              <option value="low">Low To High</option>
+              <option value="high">High To Low</option>
+            </select>
           </div>
-        </div> 
-      
-        {/* end of category */}
+        </div>
+        {/* All services with side ber */}
+        <div className="sm:flex gap-5">
+          <div className="sm:w-[250px] primary-bg p-5 border-primary rounded-l-md">
+            <h1 className="text-2xl font-semibold border-b border-b-white pb-1">
+              Filter Services
+            </h1>
+            <div className="grid grid-cols-2 gap-5 sm:block">
+              <div className="bg-white text-center mt-8 p-5 space-y-2 rounded-md">
+                <h1 className="font-semibold">Filter By Trainer</h1>
+                <select
+                  onChange={trainer}
+                  name="trainer"
+                  id="trainer"
+                  className="w-full primary-bg text-center py-1 rounded-md outline-0"
+                >
+                  <option selected disabled value="disable">
+                    Select
+                  </option>
+                  {services.map((service) => (
+                    <option key={service._id} value={service.trainer}>
+                      Jhon doe
+                    </option>
+                  ))}
+                </select>
+              </div>
 
-                        
+              {/*Category  */}
+              <h1 className="text-2xl font-semibold border-b border-b-white pb-1 mt-8  ">
+                Filter Category
+              </h1>
+              <div className="gap-2 grid-cols-2 grid  p-5">
+                <button
+                  className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
+                  // onClick={() => handleCategory("All")}
+                >
+                  All
+                </button>
+                <button
+                  className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
+                  // onClick={() => handleCategory("All")}
+                >
+                  Yoga
+                </button>
+                <button
+                  className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
+                  // onClick={() => handleCategory("Kitchen")}
+                >
+                  Weight Loss
+                </button>
+                <button
+                  className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
+                  // onClick={() => handleCategory("Bedroom")}
+                >
+                  Nutrition
+                </button>
+                <button
+                  className="text-white hover:bg-[#448c74] hover:text-[#e5c466] secondary-bg block btn  font-semibold w-20"
+                  // onClick={() => handleCategory("Living Room")}
+                >
+                  Fitness
+                </button>
+              </div>
+              <div>
+                <div>
+                  <h3 className="text-2xl font-semibold border-b border-b-white pb-1 mt-8 ">
+                    Filter Course Duration
+                  </h3>
+                  <div className="p-5">
+                    <div>
+                      <input
+                        type="radio"
+                        name="price"
+                        value="15-30"
+                        id="regular"
+
+                        // onChange={onOptionChange}
+                      />
+                      <label htmlFor="regular">15-30</label>
                     </div>
-                </div>
-      <div className="flex-1 p-5">
-      <h1 className="text-2xl font-semibold border-b border-b-white ">All Services</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-          {services.map((service) => (
+                    <div>
+                      <input
+                        type="radio"
+                        name="price"
+                        value="30-50"
+                        id="medium"
 
-            <ServiceCard key={service._id} service={service}></ServiceCard>
-          ))}
+                        // onChange={onOptionChange}
+                      />
+                      <label htmlFor="medium">30-50</label>
+                    </div>
+                    <div>
+                      <input
+                        type="radio"
+                        name="price"
+                        value="50-80"
+                        id="large"
+
+                        // onChange={onOptionChange}
+                      />
+                      <label htmlFor="large">50-80</label>
+                    </div>
+                    <div>
+                      <input
+                        type="radio"
+                        name="price"
+                        value="80-100"
+                        id="extra-large"
+
+                        // onChange={onOptionChange}
+                      />
+                      <label htmlFor="extra-large">80-100</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* end of category */}
+            </div>
+          </div>
+          <div className="flex-1 p-5">
+            <h1 className="text-2xl font-semibold border-b border-b-white ">
+              All Services
+            </h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+              {services.map((service) => (
+                <ServiceCard key={service._id} service={service}></ServiceCard>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-     
-      
-    </div>
+    </>
   );
 };
 
--- src/Pages/UsersServices/BMIServices/BMIServices.jsx
+++ src/Pages/UsersServices/BMIServices/BMIServices.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "../../../Shared/Loading/Loading";
 import ServiceCard from "../../../Components/ServicesComponents/Cards/ServiceCard";
 import Title from "../../../Shared/Title";
+import BMIServicesTitle from "./BMIServicesTitle";
 
 const BMIServices = () => {
   const [BMI, setBMI] = useState("");
@@ -17,11 +18,10 @@ const BMIServices = () => {
     queryKey: ["BMIservices"],
     queryFn: async () => {
       const res = await axiosPublic.get("/services");
-      return res.data; 
+      return res.data;
     },
   });
 
-  
   useEffect(() => {
     if (data) {
       SetAllServices(data); // Set the state outside the query function
@@ -40,7 +40,9 @@ const BMIServices = () => {
     const convertToInches = feet * 12 + inches;
     const convertToMeter = convertToInches * 0.0254;
 
-    const calculatedBMI = (weight / (convertToMeter * convertToMeter)).toFixed(2 );
+    const calculatedBMI = (weight / (convertToMeter * convertToMeter)).toFixed(
+      2
+    );
     setBMI(calculatedBMI);
     //BMI related services
     if (calculatedBMI < 18.5) {
@@ -84,65 +86,105 @@ const BMIServices = () => {
   };
 
   return (
-    <div className="px-[2%] sm:px-[5%] lg:px-[8%] py-28">
-<div className="flex items-center gap-10 md:flex-row flex-col">
-      <div>
-      <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">Find Your Exact <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-[#448c74] relative inline-block"><span className="relative text-white"> BMI</span></span></h2>
-       <p className="my-5">BMI is a useful measure of overweight and obesity. It is calculated from your height and weight. BMI is an estimate of body fat and a good gauge of your risk for diseases that can occur with more body fat.
-</p>
-      </div>
-         
-       <img src="https://i.ibb.co/cgyQMMB/BMI.jpg" alt="" className="md:w-1/2 w-full" />
-       </div>
-        <div className="mt-20">
-       <Title subHeading={"Fill up with your height and weight "}></Title>
-      
-       </div>
-       {/* Form and data */}
-       <div className="flex gap-20  md:flex-row flex-col ">
-       <form action="" onSubmit={handleBMI}>
-       <div className="flex md:flex-row flex-col md:gap-4 gap-2 items-center justify-center ">
-        <span className="secondary-color font-bold text-lg">Your Height:</span>
-      <div className="flex gap-4">
-      <input type="number" className="w-1/2 border-2  border-[#448c74] rounded-md p-2 " placeholder="Feet" name="feet" />
-        <input type="number" className="w-1/2 border-2  border-[#448c74] rounded-md p-2" placeholder="Inches" name="inches" />
-      </div>
-       </div>
-        <br />
-        <div className="flex md:flex-row flex-col md:gap-4 gap-2 items-center justify-center ">
-        <span className="secondary-color font-bold text-lg">Your Weight:</span>
-      <div className="flex gap-4">
-     
-        <input type="text" id="" placeholder="Weight" name="weight" className=" border-2  border-[#448c74] rounded-md p-2" />
-      </div>
-       </div>
-      
-        <br />
-        <div>
-        <input type="submit" name="" id="" placeholder="Submit" className="rounded-md px-4 py-2 hover:bg-white hover:border hover:border-[#448c74] hover:text-[#448c74] text-white capitalize secondary-bg md:text-2xl text-base  transition-all duration-150 ease-in-out block mx-auto"/>
-        </div>
-      </form>
-      <h2 className="text-2xl">
-        Your BMI is: <span className="font-bold">{BMI} </span> and you are  <span className="font-bold"> {text} </span>   
-      </h2>
-   
-       </div>
-      
-     
-        {
-          BMIServices.length>1 &&
-          <div className="flex-1 p-5 my-20">
-          <Title heading={'Our featured services'} subHeading={'According to your BMI'}></Title>
-          
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {BMIServices.map((service) => (
-              <ServiceCard key={service._id} service={service}></ServiceCard>
-            ))}
+    <>
+      <BMIServicesTitle></BMIServicesTitle>
+      <div className="px-[2%] sm:px-[5%] lg:px-[8%] py-28">
+        <div className="flex items-center gap-10 md:flex-row flex-col">
+          <div>
+            <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+              Find Your Exact{" "}
+              <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-[#448c74] relative inline-block">
+                <span className="relative text-white"> BMI</span>
+              </span>
+            </h2>
+            <p className="my-5">
+              BMI is a useful measure of overweight and obesity. It is
+              calculated from your height and weight. BMI is an estimate of body
+              fat and a good gauge of your risk for diseases that can occur with
+              more body fat.
+            </p>
           </div>
+
+          <img
+            src="https://i.ibb.co/cgyQMMB/BMI.jpg"
+            alt=""
+            className="md:w-1/2 w-full"
+          />
         </div>
-        }   
-    
-    </div>
+        <div className="mt-20">
+          <Title subHeading={"Fill up with your height and weight "}></Title>
+        </div>
+        {/* Form and data */}
+        <div className="flex gap-20  md:flex-row flex-col ">
+          <form action="" onSubmit={handleBMI}>
+            <div className="flex md:flex-row flex-col md:gap-4 gap-2 items-center justify-center ">
+              <span className="secondary-color font-bold text-lg">
+                Your Height:
+              </span>
+              <div className="flex gap-4">
+                <input
+                  type="number"
+                  className="w-1/2 border-2  border-[#448c74] rounded-md p-2 "
+                  placeholder="Feet"
+                  name="feet"
+                />
+                <input
+                  type="number"
+                  className="w-1/2 border-2  border-[#448c74] rounded-md p-2"
+                  placeholder="Inches"
+                  name="inches"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="flex md:flex-row flex-col md:gap-4 gap-2 items-center justify-center ">
+              <span className="secondary-color font-bold text-lg">
+                Your Weight:
+              </span>
+              <div className="flex gap-4">
+                <input
+                  type="text"
+                  id=""
+                  placeholder="Weight"
+                  name="weight"
+                  className=" border-2  border-[#448c74] rounded-md p-2"
+                />
+              </div>
+            </div>
+
+            <br />
+            <div>
+              <input
+                type="submit"
+                name=""
+                id=""
+                placeholder="Submit"
+                className="rounded-md px-4 py-2 hover:bg-white hover:border hover:border-[#448c74] hover:text-[#448c74] text-white capitalize secondary-bg md:text-2xl text-base  transition-all duration-150 ease-in-out block mx-auto"
+              />
+            </div>
+          </form>
+          <h2 className="text-2xl">
+            Your BMI is: <span className="font-bold mr-3">{BMI} </span> and you
+            are <span className="font-bold capitalize"> {text} </span>
+          </h2>
+        </div>
+
+        {BMIServices.length > 0 && (
+          <div className="flex-1 p-5 my-20">
+            <Title
+              heading={"Our featured services"}
+              subHeading={"According to your BMI"}
+            ></Title>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+              {BMIServices.map((service) => (
+                <ServiceCard key={service._id} service={service}></ServiceCard>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
