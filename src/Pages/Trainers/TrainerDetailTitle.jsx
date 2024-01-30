535615ec95080f741198af35ab89c77595d235c8
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-01-30 07:14:49 +0600

    nasted route added in the trainer detail page

--- src/Pages/Trainers/TrainerDetailTitle.jsx
+++ src/Pages/Trainers/TrainerDetailTitle.jsx
@@ -1,26 +1,21 @@
 import { IoMdArrowDropright } from "react-icons/io";
 import { Link } from "react-router-dom";
 
-const BlogTitle = () => {
+const TrainerDetailTitle = () => {
   return (
     <>
       <div className="flex justify-between px-[2%] sm:px-[5%] lg:px-[8%]">
         <div className="flex items-center my-5 cursor-pointer">
           <Link to={"/"} className="text-xl font-semibold ">
-            Blogs{" "}
+            Trainers{" "}
           </Link>
           <IoMdArrowDropright className="w-[22px]" />
-          <h1 className="text-xl font-semibold text-[#448c74]">Blog Details</h1>
+          <h1 className="text-xl font-semibold text-[#448c74]">About trainer</h1>
         </div>
-        {/* <div className="md:flex gap-5 items-center cursor-pointer hidden ">
-          <IoFitnessOutline className="text-5xl" />
-          <GrYoga />
-          <GiBoxingGloveSurprise />
-        </div> */}
       </div>
       <hr />
     </>
   );
 };
 
-export default BlogTitle;
+export default TrainerDetailTitle;
