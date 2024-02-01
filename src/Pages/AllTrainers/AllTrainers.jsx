c19a2e0193bb0f9d323d76e01ae1f6205d0892a4
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2024-02-02 00:06:06 +0600

    added search and filter in services pages

--- src/Pages/AllTrainers/AllTrainers.jsx
+++ src/Pages/AllTrainers/AllTrainers.jsx
@@ -1,6 +1,6 @@
 import { FaSearch } from "react-icons/fa";
 import Trainer from "../../Components/HomeComponents/Trainers/Trainer";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 
 const AllTrainers = () => {
   const trainers = [
