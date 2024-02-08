c26283773fca864901041c26d02ca38a9d1d2ee1
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-02-08 07:16:06 +0600

    all users and user card added and made it dynamic

---
+++ src/Pages/AllUsers/UsersCard.jsx
@@ -0,0 +1,34 @@
+
+const UsersCard = ({user}) => {
+
+    const {email, name} = user || {}; 
+
+    
+    return (
+        <>
+            <tr>
+                <td>
+                    <div className="flex items-center gap-3">
+                        <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                                <img src="https://i.ibb.co/gM3rG8G/beautiful-woman-park-last-sunny-days.jpg" alt="Avatar Tailwind CSS Component" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-bold">{name ? name : 'Yancy Tear' }</div>
+                            <div className="text-sm opacity-50">{email}</div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+                </td>
+                <th>
+                    <button className="btn btn-neutral btn-xs">details</button>
+                </th>
+            </tr>
+        </>
+    );
+};
+
+export default UsersCard;
