f873b3d0a91101d75a453731b63c7079b1ecc37c
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2024-02-12 22:02:19 +0600

    changed all users

--- src/Pages/AllUsers/AllUsers.jsx
+++ src/Pages/AllUsers/AllUsers.jsx
@@ -6,28 +6,35 @@ const AllUsers = () => {
     const [users, setUsers] = useState([]);
     const [filteredUsers, setFilteredUsers] = useState([]); 
 
+   
     useEffect(() => {
         fetch('https://revive-server-dun.vercel.app/users')
             .then(res => res.json())
             .then(data => {
-                setUsers(data)
-            }, [])
+                setUsers(data);
+                setFilteredUsers(data);
+            })
+           
+    }, []);
 
-    })
+    // const handleNormal = () => {
+    //     const normalUser = users.filter(user => user.category == 'normal' ); 
+    //     setFilteredUsers(normalUser)
+    // }
 
-    const handleNormal = () => {
-        const normalUser = users.filter(user => user.category == 'normal' ); 
-        setFilteredUsers(normalUser)
-    }
+    // const handlePremiumUsers = () => {
+    //     const premiumUser = users.filter(user => user.category == 'premium'); 
+    //     setFilteredUsers(premiumUser)
+    // }
 
-    const handlePremiumUsers = () => {
-        const premiumUser = users.filter(user => user.category == 'premium'); 
-        setFilteredUsers(premiumUser)
-    }
-
-    const handleTrainers = () => {
-        const trainers = users.filter(user => user.category == 'trainer')
-        setFilteredUsers(trainers); 
+    // const handleTrainers = () => {
+    //     const trainers = users.filter(user => user.category == 'trainer')
+    //     setFilteredUsers(trainers); 
+    // }
+    const handleFilter=(category)=>{
+       
+        const allUsers=users.filter(user=>user.category==category)
+        setFilteredUsers(allUsers)
     }
 
     return (
@@ -37,9 +44,9 @@ const AllUsers = () => {
                 <h2 className="text-3xl font-semibold text-center mt-5 mb-10">All users</h2>
 
                 <ul className="flex justify-start gap-4 text-4xl font-medium ms-4">
-                    <li onClick={handleNormal}>Normal</li>
-                    <li onClick={handlePremiumUsers}>Premium</li>
-                    <li onClick={handleTrainers}>Trainers</li>
+                    <li onClick={()=>handleFilter('normal')}>Normal</li>
+                    <li onClick={()=>handleFilter('premium')}>Premium</li>
+                    <li onClick={()=>handleFilter('trainer')}>Trainers</li>
                 </ul>
 
                 <div className="divider"></div>
