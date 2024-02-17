34b29b91ccb7069390e1d7e6932bef0f4c07c2a3
ibrahim-khalil0 <mdibrahimkhalil0183@gmail.com>
2024-02-17 19:50:34 +0600

    update user profile

--- src/Shared/Profile/Profile.jsx
+++ src/Shared/Profile/Profile.jsx
@@ -1,12 +1,29 @@
 import { Link } from "react-router-dom";
 import useAuth from "../../Hooks/useAuth";
 import useTrainer from "../../Hooks/useTrainer";
+import { useEffect, useState } from "react";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
-const Profile = ({ img }) => {
+const Profile = () => {
 
   const { isTrainer } = useTrainer()
 
   const { user } = useAuth();
+
+
+  const [userDetails, setUserDetails] = useState({})
+  const axiosPublic = useAxiosPublic()
+
+  useEffect( () => {
+    axiosPublic(`/users?email=${user?.email}`)
+    .then(res => {
+      setUserDetails(res.data)
+      console.log(res.data)
+    })
+  }, [])
+
+
+  const {name, email, photo} = userDetails
 
   return (
     <div>
@@ -15,14 +32,14 @@ const Profile = ({ img }) => {
         <div className="text-center">
           <img
             // src={user?.photoURL}
-            src={user?.photoURL}
+            src={photo}
             alt=""
             className="md:w-[100px] md:h-[100px] my-4 w-[100px] h-[100px]  rounded-full  bg-gradient-to-r p-[4px] from-teal-400 via-sky-100 to-sky-600 object-cover "
           />
         </div>
         <div className="">
           <h1 className=" text-sm font-semibold text-blue-300">
-            {user?.displayName}
+            {name}
           </h1>
           <p className=" text-sm text-blue-300">{user?.email}</p>
         </div>
