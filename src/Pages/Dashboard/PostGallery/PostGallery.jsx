01fa0627d76804ffd45df27003a71c74d797c516
ibrahim-khalil0 <mdibrahimkhalil0183@gmail.com>
2024-02-17 18:18:51 +0600

    user posted on database

--- src/Pages/Dashboard/PostGallery/PostGallery.jsx
+++ src/Pages/Dashboard/PostGallery/PostGallery.jsx
@@ -18,20 +18,22 @@ const PostGallery = () => {
         "content-type": "multipart/form-data",
       },
     });
-    console.log(res.data);
+    const image = (res.data.data.url);
+    const title = data.title
+    const category = data.category
+    const description = data.description
+
+    const gallery = {image, title, category, description}
+    console.log(gallery)
+
+    axiosPublic.post('/gallery', gallery)
+    .then(res => {
+      console.log(res.data)
+    })
+
   }
 
-  // const handleForm = async(e) => {
-  //   e.preventDefault();
-  //   const form = e.target;
-  //   const title = form.title.value;
-  //   const category = form.category.value;
-  //   const description = form.description.value;
-  //   const galleryPost = { title, category, description };
-  //   console.log(galleryPost);
-    
-
-  // };
+ 
 
   return (
     <>
@@ -51,7 +53,7 @@ const PostGallery = () => {
               name="title"
               {...register("title", { required: true })}
               className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-sky-50 peer"
-              placeholder="title "
+
               required
             />
             <label className="peer-focus:font-medium absolute text-sm text-sky-50  transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-sky-50 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
