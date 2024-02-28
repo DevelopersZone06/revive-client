b3808ab8b5c038ca46a1f1b8203d9f6fe6845e2c
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2024-02-28 21:18:03 +0600

    added the loading

--- src/Shared/Loading/AllLoading.jsx
+++ src/Shared/Loading/AllLoading.jsx
@@ -8,9 +8,13 @@ const AllLoading = () => {
     // </div>
     <>
 
-    <h1 >Be  </h1>
+  <div className="flex items-center justify-center h-screen " style={{ background: 'radial-gradient(circle, rgba(0,51,111,1) 0%, rgba(0,0,0,1) 100%)' }}>
+ <div className="flex text-sky-50">
+ <h1 >Be  </h1>
      <div className="mx-4 w-10 h-10 animate-[spin_1s_linear_infinite] rounded-full border-double border-4 border-r-0 border-l-0 border-b-sky-400 border-t-sky-700"></div>
      <h1>Healthy</h1>
+ </div>
+  </div>
      </>
   );
 };
